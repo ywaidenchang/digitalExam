b8ad495940e5f5a5ebef8943dfa47295721f309c
ywaidenchang <143078546+ywaidenchang@users.noreply.github.com>
2024-11-07 03:36:50 +0000

    list

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    return render_template("test_home.html")
 
 @app.route('/teacher')
 def teacher():
     return render_template("teacher.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
